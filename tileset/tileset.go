732446a096496234e3f0460ff48619bdd4c47519
pyshx <piyushchauhan1004@gmail.com>
2022-11-14 19:20:26 +0530

    update: implement TilsetReader

--- tileset/tileset.go
+++ tileset/tileset.go
@@ -1,7 +1,9 @@
 package tileset
 
 import (
+	"bufio"
 	"encoding/json"
+	"io"
 	"os"
 
 	"github.com/pkg/errors"
@@ -64,20 +66,38 @@ type Asset struct {
 	Extras         interface{}            `json:"extras,omitempty"`
 }
 
+type TilesetReader struct {
+	rs *bufio.Reader
+}
+
+func NewTilsetReader(r io.Reader) *TilesetReader {
+	return &TilesetReader{
+		rs: bufio.NewReader(r),
+	}
+}
+
+func (r *TilesetReader) Decode(ts *Tileset) error {
+	if err := json.NewDecoder(r.rs).Decode(&ts); err != nil {
+		return errors.Wrap(err, "failed to decode the JSON tilset data")
+	}
+	return nil
+}
+
 func (ts *Tileset) ToJson() (string, error) {
 	b, e := json.Marshal(ts)
-	return string(b), errors.Wrap(e, "failed to convert tileset to JSON")
+	return string(b), errors.Wrap(e, "failed to marshal the tilset JSON")
 }
 
 func Open(fileName string) (*Tileset, error) {
-	jsonFile, err := os.Open(fileName)
+	f, err := os.Open(fileName)
 	if err != nil {
 		return nil, errors.Wrap(err, "open failed")
 	}
-
-	var ts *Tileset
-	if err = json.NewDecoder(jsonFile).Decode(&ts); err != nil {
-		return nil, errors.Wrap(err, "failed to decode the tileset file")
+	defer f.Close()
+	tilsetReader := NewTilsetReader(f)
+	ts := new(Tileset)
+	if err := tilsetReader.Decode(ts); err != nil {
+		return nil, errors.Wrap(err, "failed to decode the tileset")
 	}
 	return ts, nil
 }
