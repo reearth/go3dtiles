554eb56891694399fd3a66c3956830012c8b2df8
pyshx <piyushchauhan1004@gmail.com>
2022-11-14 17:56:09 +0530

    fix: undo making pointer of non-array fields

--- tileset/tileset.go
+++ tileset/tileset.go
@@ -9,25 +9,25 @@ import (
 
 type Tileset struct {
 	Asset             Asset                   `json:"asset"`
-	Properties        *map[string]Schema      `json:"properties,omitempty"`
+	Properties        map[string]Schema      `json:"properties,omitempty"`
 	GeometricError    float64                 `json:"geometricError"`
 	Root              Tile                    `json:"root"`
 	ExtenstionsUsed   *[]string               `json:"extensionsUsed,omitempty"`
 	ExtensionRequired *[]string               `json:"extensionsRequired,omitempty"`
-	Extentions        *map[string]interface{} `json:"extensions,omitempty"`
-	Extras            *interface{}            `json:"extras,omitempty"`
+	Extentions        map[string]interface{} `json:"extensions,omitempty"`
+	Extras            interface{}            `json:"extras,omitempty"`
 }
 
 type Tile struct {
 	BoundingVolume      BoundingVolume          `json:"boundingVolume"`
 	ViewerRequestVolume *BoundingVolume         `josn:"viewerRequestVolume,omitempty"`
 	GeometricError      float64                 `json:"geometricError"`
-	Refine              *string                 `json:"refine,omitempty"`
+	Refine              string                 `json:"refine,omitempty"`
 	Transform           *[16]float64            `json:"transform,omitempty"`
 	Content             *Content                `json:"content,omitempty"`
 	Children            *[]Tile                 `json:"children,omitempty"`
-	Extentions          *map[string]interface{} `json:"extensions,omitempty"`
-	Extras              *interface{}            `json:"extras,omitempty"`
+	Extentions          map[string]interface{} `json:"extensions,omitempty"`
+	Extras              interface{}            `json:"extras,omitempty"`
 }
 
 type Content struct {
@@ -42,21 +42,21 @@ type BoundingVolume struct {
 	Box        *[12]float64            `json:"box,omitempty"`
 	Region     *[6]float64             `json:"region,omitempty"`
 	Sphere     *[4]float64             `json:"sphere,omitempty"`
-	Extentions *map[string]interface{} `json:"extension,omitempty"`
-	Extras     *interface{}            `json:"extras,omitempty"`
+	Extentions map[string]interface{} `json:"extension,omitempty"`
+	Extras     interface{}            `json:"extras,omitempty"`
 }
 
 type Schema struct {
-	Maximum *float64 `json:"maximum,omitempty"`
-	Minimum *float64 `json:"minimum,omitempty"`
+	Maximum float64 `json:"maximum,omitempty"`
+	Minimum float64 `json:"minimum,omitempty"`
 }
 
 type Asset struct {
 	Version        string                  `json:"version"`
-	TilesetVersion *string                 `json:"tileSetVersion,omitempty"`
+	TilesetVersion string                  `json:"tileSetVersion,omitempty"`
 	GltfUpAxis     string                  `json:"gltfUpAxis,omitempty"`
-	Extentions     *map[string]interface{} `json:"extensions,omitempty"`
-	Extras         *interface{}            `json:"extras,omitempty"`
+	Extentions     map[string]interface{} `json:"extensions,omitempty"`
+	Extras         interface{}            `json:"extras,omitempty"`
 }
 
 func (ts *Tileset) ToJson() (string, error) {
