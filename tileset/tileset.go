fb88882a0037c7875981f33c5d08ba12411eed7b
pyshx <piyushchauhan1004@gmail.com>
2022-11-14 17:47:41 +0530

    reformat: moved type definitions from down to up -> up to down.

--- tileset/tileset.go
+++ tileset/tileset.go
@@ -7,33 +7,15 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Schema struct {
-	Maximum *float64 `json:"maximum,omitempty"`
-	Minimum *float64 `json:"minimum,omitempty"`
-}
-
-type Asset struct {
-	Version        string                  `json:"version"`
-	TilesetVersion *string                 `json:"tileSetVersion,omitempty"`
-	GltfUpAxis     string                  `json:"gltfUpAxis,omitempty"`
-	Extentions     *map[string]interface{} `json:"extensions,omitempty"`
-	Extras         *interface{}            `json:"extras,omitempty"`
-}
-
-type BoundingVolume struct {
-	Box        *[12]float64            `json:"box,omitempty"`
-	Region     *[6]float64             `json:"region,omitempty"`
-	Sphere     *[4]float64             `json:"sphere,omitempty"`
-	Extentions *map[string]interface{} `json:"extension,omitempty"`
-	Extras     *interface{}            `json:"extras,omitempty"`
-}
-
-type Content struct {
-	BoundingVolume BoundingVolume `json:"boundingVolume,omitempty"`
-	// change it to "uri" https://github.com/CesiumGS/3d-tiles/tree/main/specification#content
-	URL        string                  `json:"url"`
-	Extentions *map[string]interface{} `json:"extension,omitempty"`
-	Extras     *interface{}            `json:"extras,omitempty"`
+type Tileset struct {
+	Asset             Asset                   `json:"asset"`
+	Properties        *map[string]Schema      `json:"properties,omitempty"`
+	GeometricError    float64                 `json:"geometricError"`
+	Root              Tile                    `json:"root"`
+	ExtenstionsUsed   *[]string               `json:"extensionsUsed,omitempty"`
+	ExtensionRequired *[]string               `json:"extensionsRequired,omitempty"`
+	Extentions        *map[string]interface{} `json:"extensions,omitempty"`
+	Extras            *interface{}            `json:"extras,omitempty"`
 }
 
 type Tile struct {
@@ -48,15 +30,33 @@ type Tile struct {
 	Extras              *interface{}            `json:"extras,omitempty"`
 }
 
-type Tileset struct {
-	Asset             Asset                   `json:"asset"`
-	Properties        *map[string]Schema      `json:"properties,omitempty"`
-	GeometricError    float64                 `json:"geometricError"`
-	Root              Tile                    `json:"root"`
-	ExtenstionsUsed   *[]string               `json:"extensionsUsed,omitempty"`
-	ExtensionRequired *[]string               `json:"extensionsRequired,omitempty"`
-	Extentions        *map[string]interface{} `json:"extensions,omitempty"`
-	Extras            *interface{}            `json:"extras,omitempty"`
+type Content struct {
+	BoundingVolume BoundingVolume `json:"boundingVolume,omitempty"`
+	// change it to "uri" https://github.com/CesiumGS/3d-tiles/tree/main/specification#content
+	URL        string                  `json:"url"`
+	Extentions *map[string]interface{} `json:"extension,omitempty"`
+	Extras     *interface{}            `json:"extras,omitempty"`
+}
+
+type BoundingVolume struct {
+	Box        *[12]float64            `json:"box,omitempty"`
+	Region     *[6]float64             `json:"region,omitempty"`
+	Sphere     *[4]float64             `json:"sphere,omitempty"`
+	Extentions *map[string]interface{} `json:"extension,omitempty"`
+	Extras     *interface{}            `json:"extras,omitempty"`
+}
+
+type Schema struct {
+	Maximum *float64 `json:"maximum,omitempty"`
+	Minimum *float64 `json:"minimum,omitempty"`
+}
+
+type Asset struct {
+	Version        string                  `json:"version"`
+	TilesetVersion *string                 `json:"tileSetVersion,omitempty"`
+	GltfUpAxis     string                  `json:"gltfUpAxis,omitempty"`
+	Extentions     *map[string]interface{} `json:"extensions,omitempty"`
+	Extras         *interface{}            `json:"extras,omitempty"`
 }
 
 func (ts *Tileset) ToJson() (string, error) {
